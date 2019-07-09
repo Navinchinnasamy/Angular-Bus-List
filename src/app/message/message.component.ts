3899138592419af808fb8eeefeeea44844dd81d3
Navin Chinnasamy <navinchinnasamy@gmail.com>
2019-07-09 13:46:35 +0530

    Bus List with Message component for logging and In Memory Web API
    Signed-off-by: Navin Chinnasamy <navinchinnasamy@gmail.com>

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -8,7 +8,8 @@ import { MessageService } from '../message.service';
 })
 export class MessageComponent implements OnInit {
 
-  constructor(private messageService: MessageService) { }
+    constructor(public messageService: MessageService) {
+    }
 
   ngOnInit() {
   }
